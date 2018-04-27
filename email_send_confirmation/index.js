5c67f27b24829a1651d85544331384f677457205
James Schumann <jschumann@gmail.com>
2018-04-27 11:20:38 -0400

    add more values to template; break up variable assignment

--- email_send_confirmation/index.js
+++ email_send_confirmation/index.js
@@ -8,15 +8,25 @@ module.exports = function (context, data) {
     var service_key = process.env['HedwigKey'];
     var template_path = __dirname + '/../email_send_confirmation/template.ejs';
 
-    var payload = {
-        "from":"claptrap@wrdsb.ca",
-        "to":"james_schumann@wrdsb.ca",
-        "subject":"Test HTML email from Claptrap, via Hedwig",
-        "text":"This is another test email.",
-    };
+    var from = data.from;
+    var to = data.to;
+    var subject = "Your confirmation is required";
+
     var template_values = {
+        //Please confirm <%= thing_being_confirmed %> by clicking the link below.
+        "thing_being_confirmed": "this thing that needs confirmation",
+
+        "reason_confirmation_required": "We may need to send you critical information about our service and it is important that we have an accurate email address.";
         "confirmation_button_text": "Confirm, baby!"
     };
+
+    var payload = {
+        "from": from,
+        "to": to,
+        "subject": subject
+    };
+
+    payload.text = "This is another test email.";
 
     ejs.renderFile(template_path, template_values, function(err, str) {
         if (err) {
